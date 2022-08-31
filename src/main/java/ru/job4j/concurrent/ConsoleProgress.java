4b57b572a62d8a54d0231148447051df32bd3215
PK rabota <klimenko161@gmail.com>
2022-08-31 15:52:44 +0300

    3. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+
+    }
+
+    @Override
+    public void run() {
+        int i = 0;
+        char[] processCharArray = {'-', '\\', '|', '/'};
+
+        while (!Thread.currentThread().isInterrupted()) {
+            System.out.print("\r Loading: " + processCharArray[i++]);
+            if (i == processCharArray.length) {
+                i = 0;
+            }
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
