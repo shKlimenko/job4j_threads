98f657e1e12d45f0f06a7d228b5fbfb1f9478902
PK rabota <klimenko161@gmail.com>
2022-09-03 15:05:35 +0300

    upd

---
+++ src/main/java/ru/job4j/concurrent/ThreadStop.java
@@ -0,0 +1,23 @@
+package ru.job4j.concurrent;
+
+public class ThreadStop {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println("start ...");
+                            Thread.sleep(10000);
+                        } catch (InterruptedException e) {
+                            System.out.println(Thread.currentThread().isInterrupted());
+                            System.out.println(Thread.currentThread().getState());
+                        }
+                    }
+                }
+        );
+        progress.start();
+        Thread.sleep(1000);
+        progress.interrupt();
+        progress.join();
+    }
+}
