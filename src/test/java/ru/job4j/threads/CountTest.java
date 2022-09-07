bb6c3e75ec1c8754d542b9910ee2bbb11ea88f38
Admin <klimenko161@gmail.com>
2022-09-07 20:48:41 +0300

    2. JCIP. Настройка библиотеки [#268575]

---
+++ src/test/java/ru/job4j/threads/CountTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.threads;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class CountTest {
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        var count = new Count();
+        var first = new Thread(count::increment);
+        var second = new Thread(count::increment);
+
+        first.start();
+        second.start();
+
+        first.join();
+        second.join();
+
+        assertThat(count.getValue()).isEqualTo(2);
+    }
+}
